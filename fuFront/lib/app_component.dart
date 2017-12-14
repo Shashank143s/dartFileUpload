92acf8ab75e65aa59623f8e9c395f20bba3004c2
Shashank Kumar <shashank_kum@HCL.COM>
2017-12-14 18:26:42 +0530

    3rd Commit with upload component complete and welcome screen to be made

--- fuFront/lib/app_component.dart
+++ fuFront/lib/app_component.dart
@@ -1,7 +1,8 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
-
+import 'package:angular_router/angular_router.dart';
 import 'src/fileUpload/file_upload_component.dart';
+
 
 // AngularDart info: https://webdev.dartlang.org/angular
 // Components info: https://webdev.dartlang.org/components
@@ -10,10 +11,25 @@ import 'src/fileUpload/file_upload_component.dart';
   selector: 'my-app',
   styleUrls: const ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: const [materialDirectives, FileUploadComponent],
+  directives: const [materialDirectives, ROUTER_DIRECTIVES],
   providers: const [materialProviders]
 )
 
+@RouteConfig(const [
+  const Route(path: '/fileupload', name: 'Upload', component: FileUploadComponent)
+])
+
 class AppComponent {
   // Nothing here yet. All logic is in FileUploadComponent.
+  bool selectValueUpload = false;
+  bool selectValueView = false;
+
+  void selectedIconUpload(){
+    selectValueUpload = true;
+    selectValueView = false;
+  }
+  void selectedIconView(){
+    selectValueView = true;
+    selectValueUpload = false;
+  }
 }
--- fuFront/web/main.dart
+++ fuFront/web/main.dart
@@ -1,7 +1,12 @@
 import 'package:angular/angular.dart';
+import 'package:angular_router/angular_router.dart';
 
 import 'package:fuFront/app_component.dart';
 
 void main() {
-  bootstrap(AppComponent);
+  bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    // Remove next line in production
+    provide(LocationStrategy, useClass: HashLocationStrategy),
+  ]);
 }
